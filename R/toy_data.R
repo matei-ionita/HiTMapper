e5291163e1f40b2887c7d3953e40428635569896
Matei Ionita <matei.ionita@gmail.com>
2022-12-16 15:22:00 -0500

    changed example data

---
+++ R/toy_data.R
@@ -0,0 +1,15 @@
+#' Flow cytometry measurements for 10,000 cells.
+#'
+#' A dataset containing compensated, biexponentially transformed
+#' values of 26-color flow cytometry data.
+#'
+#' @format A data frame with 10,000 rows and 26 variables:
+#' \describe{
+#'   \item{CD8}{Lineage marker for cytotoxic T cells}
+#'   \item{TCRgd}{T cell receptor gamma-delta}
+#'   \item{CD45}{Lineage marker for lymphocytes}
+#'   \item{CD4}{Lineage marker for helper T cells}
+#'   \item{CD19}{Lineage marker for B cells}
+#'   \item{CD3}{Lineage marker for T cells}
+#' }
+"toy_data"
